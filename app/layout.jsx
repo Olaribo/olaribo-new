d475f54f220d4e6bc9799e86f37115333cc3a7ad
Olaribo <Info@olaribo.com>
2026-05-21 17:23:08 +0100

    Add metadata for Olaribo Business Solution

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Olaribo Business Solution",
+  description: "Managed IT Services",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
